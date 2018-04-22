e95a8037334c41bf83cc1a53507735346b807564
Rahul-RB <rahulbharadwaj033@gmail.com>
2018-04-22 12:31:44 +0530

    Added Streak function interface

--- 2/streak.cc
+++ 2/streak.cc
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cstdint>
+#include <ctime>
+#include <cstdlib>
 
 using std::cout;
 using std::cin;
@@ -7,23 +9,59 @@ using std::endl;
 
 typedef uint64_t int_type;
 
-int main(int argc, char const *argv[])
+int_type get_streak(int_type N)
 {
 	int_type K=1;
-	int_type N;
-
-	cout<<"Enter N:";
-	cin>>N;
-
-	if(N==1){
+	if(N==1)
+	{
 		cout<<"Not defined"<<endl;
+		return -1;
 	}
-	else{
+	else if(N%2 == 0) //even number
+	{
+		return 1;
+	}
+	else // odd number, check with 2
+	{
+		if((N+2)%(3) != 0)
+		{
+			return 2;
+		}
 		while((N+K)%(K+1) == 0)
 		{
 			K++;
 		}
-		cout<<"K is:"<<K<<endl;
+		return K;
 	}
+}
+
+void test_get_streak(int_type* N)
+{
+	const clock_t begin_time = clock();
+	cout<<"N[i]\t\t\t"<<"K\n";
+	for (int_type i = 0; i < 1000000; ++i)
+	{
+		int_type res = get_streak(N[i]);
+		cout<<N[i]<<"\t"<<res<<endl;
+	}
+	
+	cout<<float( clock () - begin_time ) /  CLOCKS_PER_SEC<<" seconds"<<endl;
+
+}
+
+int main(int argc, char const *argv[])
+{
+	int_type N=atoi(argv[1]);
+	// int_type* N = new int_type[1000000];
+	int_type j = 0;
+
+	// for (int_type i = 3; i < 2000003; i=i+2)
+	// {
+	// 	N[j] = i;
+	// 	j++;
+	// }
+	int res = get_streak(N);
+	if(res!=-1)
+		cout<<res<<endl;
 	return 0;
 }
